a2a134ed2f442b3be15f074e37c407af6db9feea
PuDDDle <volodabelihin35@gmail.com>
2025-05-22 16:47:21 +0300

    start ts

---
+++ src/basic/3.ts
@@ -0,0 +1,7 @@
+let stringOrNumber: string | number;
+stringOrNumber = 10;
+stringOrNumber = "Hello!";
+
+type LiteralVariable = "enable" | "disable";
+let switcher: LiteralVariable;
+switcher = "disable";
